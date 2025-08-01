4f2ef15ca6c24e65b9d0bf06f5fac34bd2fb5310
Cedric Bauer <cbauer@2-F-2.42heilbronn.de>
2025-08-01 11:38:57 +0200

    Parsing beginning

---
+++ Parsing/inc/cub3d.h
@@ -0,0 +1,23 @@
+#ifndef CUB3D_H
+#define CUB3D_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+typedef struct s_setup
+{
+	char	**lines;
+	int		lcount;
+}	t_setup;
+
+// ----------------------------------------------------------------------
+//							  MAIN
+// ----------------------------------------------------------------------
+
+int main(int argc, char **argv);
+
+#endif
