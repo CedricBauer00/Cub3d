c1f79f88abf828f2241281a45b62eb47bc6eaec0
Cedric Bauer <cbauer@2-F-2.42heilbronn.de>
2025-08-01 13:20:53 +0200

    libft gnl added and a little work on main.c and makefile

--- Parsing/inc/cub3d.h
+++ Parsing/inc/cub3d.h
@@ -7,18 +7,39 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include "../get_next_line/get_next_line.h"
 
-typedef struct s_setup
+typedef struct s_configs
 {
 	char	**lines;
 	int		lcount;
-}	t_setup;
+
+	//textures
+	char	*no_text;
+	char	*so_text;
+	char	*we_text;
+	char	*ea_text;
+
+	//floor/ceeling
+	int		f_clr;
+	int		c_clr;
+
+	//map
+	int		m_hight;
+	int		m_width;
+	char	**map;
+
+	//player position 
+	int		x_pos;
+	int		y_pos;
+	char	plr_dir;
+}	t_configs;
 
 // ----------------------------------------------------------------------
 //							  MAIN
 // ----------------------------------------------------------------------
 
 int		main(int argc, char **argv);
-void	init_data(t_setup *data);
+void	init_data(t_configs *data);
 
 #endif
--- Parsing/src/main.c
+++ Parsing/src/main.c
@@ -6,24 +6,58 @@
 /*   By: cbauer < cbauer@student.42heilbronn.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 11:32:55 by cbauer            #+#    #+#             */
-/*   Updated: 2025/08/01 12:04:11 by cbauer           ###   ########.fr       */
+/*   Updated: 2025/08/01 13:05:49 by cbauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3d.h"
 
-void	init_data(t_setup	*data)
+void	init_data(t_configs	*data)
 {
 	data->lines = NULL;
 	data->lcount = 0;
+	data->no_text = NULL;
+	data->so_text = NULL;
+	data->we_text = NULL;
+	data->ea_text = NULL;
+	data->f_clr = -1; //initialized to 'no color'
+	data->c_clr = -1; //initialized to 'no color'
+	data->m_hight = 0;
+	data->m_width = 0;
+	data->map = NULL;
+	data->x_pos = -1; //-1 = out_of_map_pos
+	data->y_pos = -1; //-1 = out_of_map_pos
+	data->plr_dir = '\0'; //'no_direction'
+}
+
+int	read_from_file(char **argv, t_configs *data)
+{
+	int	fd;
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+		return (printf("\033[31mError: Could not read from file! \
+			Try another one.\n\033[0m"), -1);
+	if (get_next_line(fd) == NULL)
+			return (printf("\033[31mEmpty file!\n\033[0m"), -1);
+	data->lcount++;
+	
+			// while (get_next_line(fd))
+	// {
+	// 	lines = 
+	// 	data->lcount++;
+	// }
+	close (fd);
+	return (0);
 }
 
 int	main(int argc, char **argv)
 {
-	t_setup data;
+	t_configs data;
 
 	if (argc != 2)
-		return (printf("Error: Too many/few arguments!"), -1);
-	init_struct(&data);
-	
+		return (printf("Error: Too many/few arguments!\n"), -1);
+	init_data(&data);
+	if (read_from_file(&argv[1], &data) < 0)
+		return (-1);
 }
