bff3d13ab5d6dfa024a2d16caa9f2ef021531276
tunkery <tunkery@gmail.com>
2025-08-14 10:47:34 +0200

    just updtng

--- initialize.c
+++ initialize.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   initialize.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbauer < cbauer@student.42heilbronn.de>    +#+  +:+       +#+        */
+/*   By: bolcay <bolcay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 12:55:42 by batuhan           #+#    #+#             */
-/*   Updated: 2025/08/14 09:57:44 by cbauer           ###   ########.fr       */
+/*   Updated: 2025/08/14 10:19:18 by bolcay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 void	initialize(t_game *game)
 {
 	game->player->angle = PI / 2; 
-	game->player->x = 253; //map_info.player_x
-	game->player->y = 253; //map_info.player_y
+	game->player->x = 254; //map_info.player_x
+	game->player->y = 254; //map_info.player_y
 }
